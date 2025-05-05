b651698ecf8e40fe95a0ea9af1cf6aed3667ee56
Sven H <sven.hjol@protonmail.com>
2025-05-05 13:24:34 +0000

    Don't tint the clock

--- src/main/java/svenhjol/charmony/stone_chests/client/features/chest_puzzles/screens/ClockPuzzleScreen.java
+++ src/main/java/svenhjol/charmony/stone_chests/client/features/chest_puzzles/screens/ClockPuzzleScreen.java
@@ -5,7 +5,6 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
-import svenhjol.charmony.core.client.TintedGuiGraphics;
 import svenhjol.charmony.stone_chests.common.features.chest_puzzles.menus.ClockPuzzleMenu;
 
 public class ClockPuzzleScreen extends BasePuzzleScreen<ClockPuzzleMenu> {
@@ -30,13 +29,12 @@ public class ClockPuzzleScreen extends BasePuzzleScreen<ClockPuzzleMenu> {
         var image = getClockImage(time);
 
         var pose = guiGraphics.pose();
-
         pose.pushPose();
         var x = -9; // This is scaled by pose.scale()
         var y = -42; // This is scaled by pose.scale()
         pose.translate(((float) width / 2), ((float)height / 2), 1.0f);
         pose.scale(1.24f, 1.24f, 1.24f);
-        ((TintedGuiGraphics)guiGraphics).tint(tintedBg).blit(RenderType::guiTextured, image, x, y, 0.0f, 0.0f, 16, 16, 16, 16);
+        guiGraphics.blit(RenderType::guiTextured, image, x, y, 0.0f, 0.0f, 16, 16, 16, 16);
         pose.popPose();
     }
 
